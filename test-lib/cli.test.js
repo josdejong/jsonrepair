e4ab755eef924191e814299f19c57a53476ead53
Jos de Jong <wjosdejong@gmail.com>
2023-11-01 11:18:36 +0100

    chore: switch from `mocha` to `vitest` for testing

--- test-lib/cli.test.js
+++ test-lib/cli.test.js
@@ -1,4 +1,5 @@
 // Only use native node.js API's and references to ./lib here, this file is not transpiled!
+import { describe, test } from 'vitest'
 import { strictEqual } from 'assert'
 import cp from 'child_process'
 import { dirname, join } from 'path'
@@ -7,21 +8,22 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-function run (args, done) {
+function run(args, done) {
   cp.exec('node bin/cli.js ' + args, function (e, r) {
     done(e, r)
   })
 }
 
 describe('command line interface', function () {
-  it('should repair a file', function (done) {
-    const file = join(__dirname, 'data', 'invalid.json')
+  test('should repair a file', () =>
+    new Promise((resolve) => {
+      const file = join(__dirname, 'data', 'invalid.json')
 
-    run('"' + file + '"', function (e, result) {
-      const resultWithoutReturn = result.replace(/[\n\r]/g, '')
+      run('"' + file + '"', function (e, result) {
+        const resultWithoutReturn = result.replace(/[\n\r]/g, '')
 
-      strictEqual(resultWithoutReturn, '{"hello":"world"}')
-      done()
-    })
-  })
+        strictEqual(resultWithoutReturn, '{"hello":"world"}')
+        resolve()
+      })
+    }))
 })
--- test-lib/lib.test.js
+++ test-lib/lib.test.js
@@ -1,3 +1,4 @@
+import { describe, test } from 'vitest'
 import { strictEqual } from 'assert'
 import cp from 'child_process'
 import { dirname, join } from 'path'
@@ -7,43 +8,47 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 describe('lib', () => {
-  it('should load the library using CJS', (done) => {
-    const filename = join(__dirname, 'apps/cjsApp.cjs')
+  test('should load the library using CJS', () =>
+    new Promise((resolve) => {
+      const filename = join(__dirname, 'apps/cjsApp.cjs')
 
-    cp.exec(`node ${filename}`, function (error, result) {
-      strictEqual(error, null)
-      strictEqual(result, '{"name": "John"}\n')
-      done()
-    })
-  })
+      cp.exec(`node ${filename}`, function (error, result) {
+        strictEqual(error, null)
+        strictEqual(result, '{"name": "John"}\n')
+        resolve()
+      })
+    }))
 
-  it('should load the library using ESM', (done) => {
-    const filename = join(__dirname, 'apps/esmApp.mjs')
+  test('should load the library using ESM', () =>
+    new Promise((resolve) => {
+      const filename = join(__dirname, 'apps/esmApp.mjs')
 
-    cp.exec(`node ${filename}`, function (error, result) {
-      strictEqual(error, null)
-      strictEqual(result, '{"name": "John"}\n')
-      done()
-    })
-  })
+      cp.exec(`node ${filename}`, function (error, result) {
+        strictEqual(error, null)
+        strictEqual(result, '{"name": "John"}\n')
+        resolve()
+      })
+    }))
 
-  it('should load the library using UMD bundle', (done) => {
-    const filename = join(__dirname, 'apps/umdApp.cjs')
+  test('should load the library using UMD bundle', () =>
+    new Promise((resolve) => {
+      const filename = join(__dirname, 'apps/umdApp.cjs')
 
-    cp.exec(`node ${filename}`, function (error, result) {
-      strictEqual(error, null)
-      strictEqual(result, '{"name": "John"}\n')
-      done()
-    })
-  })
+      cp.exec(`node ${filename}`, function (error, result) {
+        strictEqual(error, null)
+        strictEqual(result, '{"name": "John"}\n')
+        resolve()
+      })
+    }))
 
-  it('should load the library using minified UMD bundle', (done) => {
-    const filename = join(__dirname, 'apps/umdAppMin.cjs')
+  test('should load the library using minified UMD bundle', () =>
+    new Promise((resolve) => {
+      const filename = join(__dirname, 'apps/umdAppMin.cjs')
 
-    cp.exec(`node ${filename}`, function (error, result) {
-      strictEqual(error, null)
-      strictEqual(result, '{"name": "John"}\n')
-      done()
-    })
-  })
+      cp.exec(`node ${filename}`, function (error, result) {
+        strictEqual(error, null)
+        strictEqual(result, '{"name": "John"}\n')
+        resolve()
+      })
+    }))
 })
