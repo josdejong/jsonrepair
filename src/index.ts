b07f6f9da9cb0b2fe556014beefc0d02a96fdf55
Jos de Jong <wjosdejong@gmail.com>
2023-11-30 11:19:51 +0100

    fix: do not export `jsonrepairCore`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 // Cross-platform JavaScript API
-export { jsonrepairCore, type JsonRepairCore, type JsonRepairCoreOptions } from './core.js'
 export { jsonrepair, type JsonRepairOptions } from './jsonrepair.js'
 export { JSONRepairError } from './JSONRepairError.js'
